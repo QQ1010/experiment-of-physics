304c191e9dc571bb78dafb76ef075d792bb00605
QQ <asdfg335522@gmail.com>
2022-10-29 14:23:40 +0800

    Toolbar

---
+++ Assets/Script/ToolBar.cs
@@ -0,0 +1,152 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolBar : MonoBehaviour
+{
+    public List<GameObject> ToolbarManager = new List<GameObject>();
+    public List<GameObject> Tools;
+    public void CreateAmmeter()
+    {
+
+        // Target
+        GameObject targetObject = Tools[0];
+        // Parent
+        GameObject parentObject = GameObject.Find("Tool");
+
+        // Create
+       ToolbarManager.Add(
+           Instantiate(
+            targetObject,
+            parentObject.transform.position,
+            parentObject.transform.rotation,
+            parentObject.transform
+       ));
+    }
+    public void CreateVoltmeter()
+    {
+
+        // Target
+        GameObject targetObject = Tools[1];
+        // Parent
+        GameObject parentObject = GameObject.Find("Tool");
+
+        // Create
+        ToolbarManager.Add(
+           Instantiate(
+            targetObject,
+            parentObject.transform.position,
+            parentObject.transform.rotation,
+            parentObject.transform
+       ));
+    }
+    public void CreateGaussmeter()
+    {
+
+        // Target
+        GameObject targetObject = Tools[2];
+        // Parent
+        GameObject parentObject = GameObject.Find("Tool");
+
+        // Create
+        ToolbarManager.Add(
+           Instantiate(
+            targetObject,
+            parentObject.transform.position,
+            parentObject.transform.rotation,
+            parentObject.transform
+       ));
+    }
+    public void CreateDCPower()
+    {
+
+        // Target
+        GameObject targetObject = Tools[3];
+        // Parent
+        GameObject parentObject = GameObject.Find("Tool");
+
+        // Create
+        ToolbarManager.Add(
+           Instantiate(
+            targetObject,
+            parentObject.transform.position,
+            parentObject.transform.rotation,
+            parentObject.transform
+       ));
+    }
+    public void CreateVariableResistor()
+    {
+
+        // Target
+        GameObject targetObject = Tools[4];
+        // Parent
+        GameObject parentObject = GameObject.Find("Tool");
+
+        // Create
+        ToolbarManager.Add(
+           Instantiate(
+            targetObject,
+            parentObject.transform.position,
+            parentObject.transform.rotation,
+            parentObject.transform
+       ));
+    }
+    public void CreateRuler()
+    {
+
+        // Target
+        GameObject targetObject = Tools[5];
+        // Parent
+        GameObject parentObject = GameObject.Find("Tool");
+
+        // Create
+        ToolbarManager.Add(
+           Instantiate(
+            targetObject,
+            parentObject.transform.position,
+            parentObject.transform.rotation,
+            parentObject.transform
+       ));
+    }
+    public void CreateWireA()
+    {
+
+        // Target
+        GameObject targetObject = Tools[6];
+        // Parent
+        GameObject parentObject = GameObject.Find("Tool");
+
+        // Create
+        ToolbarManager.Add(
+           Instantiate(
+            targetObject,
+            parentObject.transform.position,
+            parentObject.transform.rotation,
+            parentObject.transform
+       ));
+    }
+    public void CreateWireB()
+    {
+
+        // Target
+        GameObject targetObject = Tools[7];
+        // Parent
+        GameObject parentObject = GameObject.Find("Tool");
+
+        // Create
+        ToolbarManager.Add(
+           Instantiate(
+            targetObject,
+            parentObject.transform.position,
+            parentObject.transform.rotation,
+            parentObject.transform
+       ));
+    }
+    public void Reset()
+    {
+        foreach (GameObject obj in ToolbarManager)
+        {
+            Destroy(obj);
+        }
+    }
+}
