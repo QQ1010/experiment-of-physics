de5310c63120c770ba62a232cec7689eba434d58
QQ <asdfg335522@gmail.com>
2023-04-25 15:48:39 +0800

    Update: student

--- Assets/Script/Acid/AddVolume.cs
+++ Assets/Script/Acid/AddVolume.cs
@@ -5,17 +5,6 @@ using UnityEngine;
 public class AddVolume: MonoBehaviour
 {
     // Start is called before the first frame update
-    public void addone()
-    {
-        Manager.instance.volume += 0.01f;
-    }
-    public void addten()
-    {
-        Manager.instance.volume += 0.1f;
-    }
-    public void addhundred()
-    {
-        Manager.instance.volume += 1f;
-    }
+    
 
 }
--- Assets/Script/Acid/Manager.cs
+++ Assets/Script/Acid/Manager.cs
@@ -2,32 +2,185 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+
 
 public class Manager : MonoBehaviour
 {
-    public Dropdown dropdown;
+    public Dropdown titrantion_dropdown;
     public static Manager instance;
     public LiquidData data;
-    public float volume;
     public int titration_solution = 0;         // 0:KOH, 1: NaOH, 2:HCL 3:H2SO4
-    public InputField titration_mole_text;
-    public float titration_mole = 0.5f;
+    public double titration_concentration = 0.5;
+    public InputField titration_concentration_text;
+    public double volume;
     public Text volume_text;
+    public double H_mole;
+    public double OH_mole;
+    bool titrantion_H = false;
+    bool liquid_H = false;
+    double pH = 7;
+    public Text PH;
     private void Awake() {
         instance = this;
         data = DataManager.data;
         volume = data.volume;
+        switch (data.solution)
+        {
+            case 0:
+                H_mole = 0;
+                OH_mole = data.concentration * (data.volume / 1000);
+                break;
+            case 1:
+                H_mole = 0;
+                OH_mole = data.concentration * (data.volume / 1000);
+                break;
+            case 2:
+                H_mole = data.concentration * (data.volume / 1000);
+                OH_mole = 0;
+                liquid_H = true;
+                break;
+            case 3:
+                H_mole = data.concentration * (data.volume / 1000) * 2;
+                OH_mole = 0;
+                liquid_H = true;
+                break;
+        }
         SetupVolume();
     }
+
+    public void addvolume(float add_volume)
+    {
+        volume += add_volume;
+        double H_mole_add = 0d;
+        double OH_mole_add = 0d;
+        switch (titrantion_dropdown.value)
+        {
+            case 0:
+                H_mole_add = 0;
+                OH_mole_add = titration_concentration * (add_volume / 1000);
+                break;
+            case 1:
+                H_mole_add = 0;
+                OH_mole_add = titration_concentration * (add_volume / 1000);
+                break;
+            case 2:
+                H_mole_add = titration_concentration * (add_volume / 1000);
+                OH_mole_add = 0;
+                titrantion_H = true;
+                break;
+            case 3:
+                H_mole_add = titration_concentration * (add_volume / 1000) * 2;
+                OH_mole_add = 0;
+                titrantion_H = true;
+                break;
+        }
+        print(titrantion_H);
+        print(liquid_H);
+        print(volume);
+        print(H_mole);
+        print(OH_mole);
+        if (titrantion_H & liquid_H)
+        {
+            H_mole += H_mole_add;
+            if (H_mole < (1e-7))
+            {
+                pH = -Math.Log((H_mole + 1e-7) / volume);
+            }
+            else
+            {
+                pH = -Math.Log(H_mole / volume);
+            }
+        }
+        else if(!titrantion_H & !liquid_H)
+        {
+            OH_mole += OH_mole_add;
+            if (OH_mole < (1e-7))
+            {
+                pH = 14 - (-Math.Log((OH_mole + 1e-7) / volume));
+            }
+            else
+            {
+                pH = 14 - (-Math.Log(OH_mole / volume));
+            }
+        }
+        else if (titrantion_H & !liquid_H)
+        {
+            OH_mole -= H_mole_add;
+            //H_mole += H_mole_add;
+            if (OH_mole < 0)
+            {
+                H_mole = -OH_mole;
+                if (H_mole < (1e-7))
+                {
+                    pH = Math.Log((H_mole + 1e-7) / volume);
+                }
+                else
+                {
+                    pH = Math.Log((H_mole) / volume);
+                }
+            }
+            else if (OH_mole == 0)
+            {
+                pH = 7;
+            }
+            else
+            {
+                if (OH_mole < (1e-7))
+                {
+                    pH = 14 - Math.Log((OH_mole + 1e-7) / volume);
+                }
+                else
+                {
+                    pH = 14 - Math.Log((OH_mole) / volume);
+                }
+            }
+        }
+        else if (!titrantion_H & liquid_H)
+        {
+            H_mole -= OH_mole_add;
+            if (H_mole < 0)
+            {
+                OH_mole = -H_mole;
+                if (OH_mole < (1e-7))
+                {
+                    pH = 14 - Math.Log((OH_mole + 1e-7) / volume);
+                }
+                else
+                {
+                    pH = 14 - Math.Log((OH_mole) / volume);
+                }
+            }
+            else if(H_mole == 0)
+            {
+                pH = 7;
+            }
+            else
+            {
+                if (H_mole < (1e-7))
+                {
+                    pH = Math.Log((H_mole + 1e-7) / volume);
+                }
+                else
+                {
+                    pH = Math.Log((H_mole) / volume);
+                }
+            }
+        }
+        PH.text = Math.Round(pH,4).ToString();
+        print("pH = " + pH);
+    }
+
     
+
     public void SetupVolume()
     {
-        volume_text.text = volume.ToString();
+        volume_text.text = Math.Round(volume,2).ToString();
     }
 
     public void SetUpType()
     {
-        switch (dropdown.value)
+        switch (titrantion_dropdown.value)
         {
             case 0:
                 titration_solution = 0;
@@ -45,14 +198,17 @@ public class Manager : MonoBehaviour
     }
     public void SetUpMole()
     {
-        titration_mole = float.Parse(titration_mole_text.text);
+        titration_concentration = float.Parse(titration_concentration_text.text);
     }
     public void Reset()
     {
         data = DataManager.data;
         volume = data.volume;
         volume_text.text = volume.ToString();
-        titration_mole_text.text = 0.5f.ToString();
-        titration_mole = 0.5f;
+        titration_concentration_text.text = 0.5f.ToString();
+        titration_concentration = 0.5f;
     }
 }
+
+
+//tpKgD + SsLG9 + rtAnGaUWjA ==
