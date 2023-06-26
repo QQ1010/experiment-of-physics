3194a09666b8e89841806c6e98327e606e869058
QQ <asdfg335522@gmail.com>
2023-06-26 22:32:03 +0800

    Fix: welgl

--- Assets/Script/experience 3/Menu.cs
+++ Assets/Script/experience 3/Menu.cs
@@ -7,7 +7,7 @@ public class Menu : MonoBehaviour
 {
     public void OnStart(int ScenceIndex)
     {
-        if (ScenceIndex != 0 || ScenceIndex != 1 || ScenceIndex != 2 || ScenceIndex != 3)
+        if (ScenceIndex == -1)
         {
             Application.Quit();
         }
