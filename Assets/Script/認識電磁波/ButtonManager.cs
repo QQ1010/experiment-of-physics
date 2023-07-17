f0983fa6d24162e3b0ebf776d32041d6a57b65eb
QQ <asdfg335522@gmail.com>
2023-07-17 21:51:14 +0800

    Update: Button fix

--- Assets/Script/認識電磁波/ButtonManager.cs
+++ Assets/Script/認識電磁波/ButtonManager.cs
@@ -6,51 +6,54 @@ using TMPro;
 
 public class ButtonManager : MonoBehaviour
 {
-    public TMP_Text button2_text;
-    public Button Button2;
-    public bool counter2 = true;
-    public TMP_Text button1_text;
-    public Button Button1;
-    public bool counter1 = true;
-    // Start is called before the first frame update
-    public void ChangeTextButton2()
+    public TMP_Text button_text;
+    public Button Button;
+    public bool counter = true;
+    public string txt_true;
+    public string txt_false;
+    public void ChangeTextButton()
     {
-        ColorBlock Color2 = Button2.colors;
-        if (counter2 == true)
+        ColorBlock buttoncolor = Button.colors;
+        if (counter == true)
         {
-            button2_text.text = "°¾·¥¬] 2: ¶}";
-            Color2.selectedColor = new Color(251, 255, 165, 255);
-            Button2.colors = Color2;
-            Debug.Log(Color2.selectedColor);
-            counter2 = false;
+            button_text.text = txt_true;
+            buttoncolor.normalColor = Color.yellow;
+            buttoncolor.selectedColor = Color.yellow;
+            buttoncolor.pressedColor = Color.yellow;
+            Button.colors = buttoncolor;
+            counter = false;
         }
         else
         {
-            button2_text.text = "°¾·¥¬] 2: Ãö";
-            Color2.selectedColor = new Color(255, 255, 255, 255);
-            Button2.colors = Color2;
-            Debug.Log(Color2.selectedColor);
-            counter2 = true;
+            button_text.text = txt_false;
+            buttoncolor.normalColor = Color.white;
+            buttoncolor.selectedColor = Color.white;
+            buttoncolor.pressedColor = Color.white;
+            Button.colors = buttoncolor;
+            counter = true;
         }
     }
-    public void ChangeTextButton1()
-    {
-        ColorBlock Color1 = Button1.colors;
-        if (counter1 == true)
-        {
-            button1_text.text = "°¾·¥¬] 1: ¶}";
-            Color1.selectedColor = new Color(251, 255, 165, 255);
-            Button1.colors = Color1;
-            Debug.Log(Color1.selectedColor);
-            counter1 = false;
-        }
-        else
-        {
-            button1_text.text = "°¾·¥¬] 1: Ãö";
-            Color1.selectedColor = new Color(255, 255, 255, 255);
-            Button1.colors = Color1;
-            Debug.Log(Color1.selectedColor);
-            counter1 = true;
-        }
-    }
+    //public void ChangeTextButton1()
+    //{
+    //    ColorBlock Color1 = Button1.colors;
+    //    if (counter1 == true)
+    //    {
+    //        button1_text.text = "°¾·¥¬] 1: ¶}";
+    //        Color1.normalColor = Color.yellow;
+    //        Color1.selectedColor = Color.yellow;
+    //        Color1.pressedColor = Color.yellow;
+    //        Button1.colors = Color1;
+    //        Debug.Log(Color1.selectedColor);
+    //        counter1 = false;
+    //    }
+    //    else
+    //    {
+    //        button1_text.text = "°¾·¥¬] 1: Ãö";
+    //        Color1.normalColor = Color.white;
+    //        Color1.selectedColor = Color.white;
+    //        Color1.pressedColor = Color.white;
+    //        Button1.colors = Color1;
+    //        counter1 = true;
+    //    }
+    //}
 }
